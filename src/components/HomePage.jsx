5ea84ece402ac75ab66f4ca020961fbac06f8798
Filip Koza <peacefilip1989@gmail.com>
2021-05-29 11:22:58 +0200

    material-ui

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,12 @@
+import ContactPhoneTwoToneIcon from '@material-ui/icons/ContactPhoneTwoTone';
+
 const HomePage = () => {
   return (
-    <h2 className="main_name">Головна сторінка сервісу "Контактна книга"</h2>
+    <>
+      <h2 className="main_name">
+        Головна сторінка сервісу "Контактна книга" <ContactPhoneTwoToneIcon />
+      </h2>
+    </>
   );
 };
 
